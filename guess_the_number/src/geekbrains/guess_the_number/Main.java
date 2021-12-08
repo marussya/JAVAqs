58af37b7032f61f0f4e345101fee791c302da947
marussya <89848161+marussya@users.noreply.github.com>
2021-12-08 12:33:33 +0300

    Выполнено задание к четвертому видео

--- guess_the_number/src/geekbrains/guess_the_number/Main.java
+++ guess_the_number/src/geekbrains/guess_the_number/Main.java
@@ -4,11 +4,18 @@ import java.util.Scanner;
 
 public class Main {
 
+    private static final Scanner scanner = new Scanner(System.in);
+
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
+
         System.out.println("Ваша задача угадать число.");
         int range = 100;
         int number = (int)(Math.random() * range);
+        playLevel(range, number);
+        scanner.close();
+    }
+
+    private static void playLevel (int range, int number) {
         while (true) {
             System.out.println("Угадайте число от 0 до " + range);
             int input_number = scanner.nextInt();
@@ -21,6 +28,5 @@ public class Main {
                 System.out.println("Загаданное число больше.");
             }
         }
-        scanner.close();
     }
 }
