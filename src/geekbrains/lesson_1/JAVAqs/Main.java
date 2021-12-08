3b771ef982af697cac70aa8b2ea346764f51fea3
marussya <89848161+marussya@users.noreply.github.com>
2021-12-08 11:33:17 +0300

    Выполнено задание к первому видео

--- src/geekbrains/lesson_1/JAVAqs/Main.java
+++ src/geekbrains/lesson_1/JAVAqs/Main.java
@@ -3,6 +3,6 @@ package geekbrains.lesson_1.JAVAqs;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        System.out.println("Я буду изучать JAVA");
     }
 }
